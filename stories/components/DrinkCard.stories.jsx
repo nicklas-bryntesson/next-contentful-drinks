db34065cb7fe6df83d7d5f6d06f52352902b95c6
Nicklas Bryntesson <nicklas.bryntesson@chas.se>
2022-01-19 22:15:37 +0100

    Add storybook to project

---
+++ stories/components/DrinkCard.stories.jsx
@@ -0,0 +1,8 @@
+/* import DrinkCard from '../../components/DrinkCard';
+
+export default {
+    title: "Components/DrinkCard",
+    component: DrinkCard,
+  };
+  
+  export const DrinkCardStory = () => <DrinkCard /> */
---
+++ stories/pages/recepies.stories.jsx
@@ -0,0 +1,8 @@
+/* import Recipes from "../../pages/index";
+
+export default {
+  title: "Pages/Home",
+  component: Recipes,
+};
+
+export const HomePage = () => <Recipes /> */
